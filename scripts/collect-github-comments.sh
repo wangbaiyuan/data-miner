2b3b9b2547882693f6057dc306ef3814f1d57873
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-18 14:58:31 +0800

    fix collect github comments

--- scripts/collect-github-comments.sh
+++ scripts/collect-github-comments.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 echo "Collecting comments from github issue..."
-node ./src/services/githubService.js > "_data/github-comments.json"
+node ./build/services/githubService.js > "_data/github-comments.json"
