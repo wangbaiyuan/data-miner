75893bbda9ab9dfc4397f1f8592b03cf089b98d9
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-20 09:11:01 +0800

    (feat) ignore failed job

--- scripts/collect-wordpress-comments.sh
+++ scripts/collect-wordpress-comments.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 echo "Collecting comments from Wordpress..."
 curl -u "github-action:${WP_TOKEN}" ${WP_COMMENTS_API_URL}\
  -f -s -w "Response: %{http_code}\n" -o "_data/wp-comments.json"
